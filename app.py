8490471203695aa37c02d38630f4e7c9fa839e2f
Arjun <cosmicpepe2001@gmail.com>
2020-05-09 21:42:18 +0530

    home form

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY 
 
 @app.route("/")
-def home():
-    return render_template('home.html')
-
-@app.route("/datainput")
 def dataentry():
     form = cardioForm()
     return render_template('form.html',form=form)
