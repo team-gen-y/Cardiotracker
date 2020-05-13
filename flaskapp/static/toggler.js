5882c449030124967acf98c8d3ab09d306d37a6f
Arjun <cosmicpepe2001@gmail.com>
2020-05-13 23:59:57 +0530

    login/reg_static

--- flaskapp/static/toggler.js
+++ flaskapp/static/toggler.js
@@ -1,16 +1,27 @@
 const toggler=document.querySelector('#top-nav-toggler')
 const splashImage = document.querySelector('.masthead')
-var state = 1;
+const loginToggler = document.querySelector('#navbarSignIn');
+const page = document.querySelector('html');
+const signUpForm = document.querySelector('#signup');
+const homeForm = document.querySelector('#home-form');
+var maststate = 1;
+var logState = -1;
 
 toggler.onclick = function(){
-    state = state*-1;
-    stateModifier();
+    maststate = maststate*-1;
+    maststateModifier();
+}
+function maststateModifier()
+{
+    if(maststate==-1){
+        splashImage.style.display='none';
+        document.getElementById('top-right-user').style.visibility='hidden';
+    }
+    else{
+        splashImage.style.display='flex';
+        document.getElementById('top-right-user').style.visibility='visible';
+
+    }
+
 }
 
-function stateModifier()
-{
-    if(state==-1)
-        splashImage.style.display='none';
-    else
-        splashImage.style.display='flex';
-}
