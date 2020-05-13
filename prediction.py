11dc0cc056040592e28a7d2c76b9dd88f745b28b
Sujay Goswami <sujaygoswami@hotmail.com>
2020-05-13 20:31:31 +0530

    visualizations uploaded

---
+++ prediction.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+df = pd.read_csv('cardio_train.csv', sep = ';')
+df.drop("id",axis=1,inplace=True)
+df.drop_duplicates(inplace=True)
+df['years'] = (df['age'] / 365).round(0)
+df['years'] = pd.to_numeric(df['years'], downcast = 'integer')
+df['Cardio'] = df['cardio']
+df = df.drop('age', axis = 1)
+df = df.drop('cardio', axis = 1)
+
+X = df.iloc[:, :-1].values
+Y = df.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 1)
+
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 1)
+forest.fit(X_train, Y_train)
+model = forest
+
+model.predict(np.array)
+
---
+++ visuals.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as mp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('user_data.csv')
+
+def smokeperday(ID, df):
+    data = df.loc[df.id == ID]
+    plt.plot(data.date, data.num_smoke, color = 'brown')
+    plt.xticks(rotation = 90)
+    plt.xlabel('date')
+    plt.ylabel('number of cigarettes')
+    plt.grid()
+    plt.show()
+    #g = sns.lineplot(x = data.date, y = data.num_smoke, color = 'brown').set_xticklabels(df.date, rotation = 90)
+    
+def bpdisp(ID, df):
+    data = df.loc[df.id == ID]
+    plt.plot(data.date, data.ap_lo, color = 'yellow', label = 'diastolic')
+    plt.plot(data.date, data.ap_hi, color = 'blue', label= 'systolic')
+    plt.legend(loc="upper right")
+    plt.xticks(rotation = 90)
+    plt.xlabel('date')
+    plt.ylabel('blood pressure')
+    plt.grid()
+    plt.show()
+    #sns.lineplot(x = data.date, y = data.ap_lo, color = 'blue').set_xticklabels(df.date, rotation = 90)
+    #sns.lineplot(x = data.date, y = data.ap_hi, color = 'yellow').set_xticklabels(df.date, rotation = 90)
+    
+def alcoperday(ID, df):
+    data = df.loc[df.id == ID]
+    plt.plot(data.date, data.num_alco, color = 'red')
+    plt.xticks(rotation = 90)
+    plt.xlabel('date')
+    plt.ylabel('liters of alcohol consumed')
+    plt.grid()
+    plt.show()
+    
+def activtime(ID, df):
+    data = df.loc[df.id == ID]
+    plt.plot(data.date, data.activ_time, color = 'green')
+    plt.xticks(rotation = 90)
+    plt.xlabel('date')
+    plt.ylabel('hours of physical activity')
+    plt.grid()
+    plt.show()
+    
+def bminweight(ID, df):
+    data = df.loc[df.id == ID]
+    plt.plot(data.date, data.bmi, color = 'black', label = 'bmi')
+    plt.plot(data.date, data.weight, color = 'purple', label= 'weight')
+    plt.legend(loc="upper right")
+    plt.xticks(rotation = 90)
+    plt.xlabel('date')
+    plt.ylabel('blood pressure')
+    plt.grid()
+    plt.show()
+    
+smokeperday() #put parameters customer_id and 'df'
+bpdisp() #put parameters customer_id and 'df'
+alcoperday() #put parameters customer_id and 'df'
+activtime() #put parameters customer_id and 'df'
+bminweight() #put parameters customer_id and 'df'
+
