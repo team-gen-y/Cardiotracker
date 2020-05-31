f169dd1f51b1f7e207965aa628ee44e9833e5168
sg23600 <sujaygoswami@hotmail.com>
2020-05-31 16:06:36 +0530

    dynamic visuals added

--- visuals.py
+++ visuals.py
@@ -1,103 +1,107 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-import numpy as mp
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-from csv import writer
-
-def append_list_as_row(file_name, list_of_elem):
-	with open(file_name, list_of_elem):
-		csv_writer = writer(write_obj)
-		csv_writer.writerow(list_of_elem)
-		
+import plotly.graph_objs as go
+import plotly.io as pio
+import plotly.express as px
 
 df = pd.read_csv('user_data.csv')
 
-#use the functio below 
-def new_record(record):
-	append_list_as_row('user_data.csv', record)
+df['bp_cat'] = ""
 
-def smokeperday(ID, df):
-    data = df.loc[df.id == ID]
-    plt.plot(data.date, data.num_smoke, color = 'brown')
-    plt.xticks(rotation = 90)
-    plt.xlabel('date')
-    plt.ylabel('number of cigarettes')
-    plt.grid()
-    plt.show()
-    #g = sns.lineplot(x = data.date, y = data.num_smoke, color = 'brown').set_xticklabels(df.date, rotation = 90)
-    
-def bpdisp(ID, df):
-    data = df.loc[df.id == ID]
-    plt.plot(data.date, data.ap_lo, color = 'yellow', label = 'diastolic')
-    plt.plot(data.date, data.ap_hi, color = 'blue', label= 'systolic')
-    plt.legend(loc="upper right")
-    plt.xticks(rotation = 90)
-    plt.xlabel('date')
-    plt.ylabel('blood pressure')
-    plt.grid()
-    plt.show()
-    #sns.lineplot(x = data.date, y = data.ap_lo, color = 'blue').set_xticklabels(df.date, rotation = 90)
-    #sns.lineplot(x = data.date, y = data.ap_hi, color = 'yellow').set_xticklabels(df.date, rotation = 90)
-    
-def bp_lo(ID, df):
-	data = df.loc[df.id == ID]
-	plt.plot(data.date, data.ap_lo, color = 'yellow')
-	plt.xticks(rotation = 90)
-	plt.xlabel('date')
-	plt.ylabel('BP')
-	plt.grid()
-	plt.show()
-	
-def bp_hi(ID, df):
-	data = df.loc[df.id == ID]
-	plt.plot(data.date, data.ap_hi, color = 'blue')
-	plt.xticks(rotation = 90)
-	plt.xlabel('date')
-	plt.ylabel('BP')
-	plt.grid()
-	plt.show()
+for i in range(len(df)):
+    if df.ap_lo[i]<=60 and df.ap_hi[i]<=100:
+        df.bp_cat[i] = 'low'
+    elif (df.ap_lo[i]>60 and df.ap_lo[i]<=80) or (df.ap_hi[i]>100 and df.ap_hi[i]<=120):
+        df.bp_cat[i] = 'normal'
+    elif (df.ap_lo[i]>80) or (df.ap_hi[i]>120):
+        df.bp_cat[i] = 'high'
 
-def alcoperday(ID, df):
-    data = df.loc[df.id == ID]
-    plt.plot(data.date, data.num_alco, color = 'red')
-    plt.xticks(rotation = 90)
-    plt.xlabel('date')
-    plt.ylabel('liters of alcohol consumed')
-    plt.grid()
-    plt.show()
-    
-def activtime(ID, df):
-    data = df.loc[df.id == ID]
-    plt.plot(data.date, data.activ_time, color = 'green')
-    plt.xticks(rotation = 90)
-    plt.xlabel('date')
-    plt.ylabel('hours of physical activity')
-    plt.grid()
-    plt.show()
-    
-def bminweight(ID, df):
-    data = df.loc[df.id == ID]
-    plt.plot(data.date, data.bmi, color = 'black', label = 'bmi')
-    plt.plot(data.date, data.weight, color = 'purple', label= 'weight')
-    plt.legend(loc="upper right")
-    plt.xticks(rotation = 90)
-    plt.xlabel('date')
-    plt.ylabel('blood pressure')
-    plt.grid()
-    plt.show()
-    
-#smokeperday() #put parameters customer_id and 'df'
-bpdisp() #put parameters customer_id and 'df'
-bp_lo()
-bp_hi()
-#alcoperday() #put parameters customer_id and 'df'
-#activtime() #put parameters customer_id and 'df'
-bminweight() #put parameters customer_id and 'df'
+df['bmi_cat'] = ""
 
+for i in range(len(df)):
+    if df.bmi[i] < 18.5:
+        df.bmi_cat[i] = 'underweight'
+    elif df.bmi[i] >= 18.5 and df.bmi[i] <= 24.9:
+        df.bmi_cat[i] = 'healthy weight'
+    else:
+        df.bmi_cat[i] = 'obese'
+
+df['bp_sum'] = ""
+
+for i in range(len(df)):
+    df.bp_sum[i] = float(0.25 * (df.ap_lo[i] + df.ap_hi[i]))
+    
+df.bp_sum = pd.to_numeric(df.bp_sum)
+
+#empty the csv file
+def clear_all():
+    global df
+    df = df[0:0]
+    df.to_csv('user_data.csv')
+
+
+#3D display of blood pressures
+def BP_display(df):
+    trace1 = go.Scatter3d(
+        x=df.ap_lo,
+        y=df.ap_hi,
+        z=df.date,
+        mode='markers',
+        marker=dict(
+            size=10,
+            color='rgb(255,0,0)',    # set color to an array/list of desired values      
+        )
+    )
+
+    data = [trace1]
+    layout = go.Layout(
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0  
+        )
+    
+    )  
+    fig = go.Figure(data=data, layout=layout)
+    fig = px.scatter_3d(df, x = 'ap_lo', y = 'ap_hi', z = 'date', color = 'bp_cat', size = 'bp_sum', size_max = 100, hover_name = 'bp_cat')
+    pio.show(fig)
+
+#Scatter Display of pulse rates
+def Pulse_Display(df):
+    fig = px.scatter(df, x="date", y="pulse", size = 'pulse', size_max = 60)
+    fig.show()
+
+#Scatter Display of BMI with hovering weights
+def BMI_Display(df):
+    fig = px.scatter(df, x = 'date', y = 'bmi', color = 'bmi_cat', size = 'bmi', size_max = 60, hover_name = 'weight')
+    fig.show()
+
+#Linear Display of smokes per day
+def Smoke_Display(df):
+    fig = px.line(df, x = 'date', y = 'num_smoke')
+    fig.show()
+
+#Linear Display of alcohol per day
+def Alcohol_Display(df):
+    fig = px.line(df, x = 'date', y = 'num_alco')
+    fig.show()
+
+#Linear Display of active time per day
+def Activity_Display(df):
+    fig = px.line(df, x = 'date', y = 'active_time')
+    fig.show()
+
+########## ALL FUNCTOINS ##############
+#3D display of blood pressures
+BP_display(df)
+#Scatter Display of pulse rates
+Pulse_Display(df)
+#Scatter Display of BMI with hovering weights
+BMI_Display(df)
+#Linear Display of smokes per day
+Smoke_Display(df)
+#Linear Display of alcohol per day
+Alcohol_Display(df)
+#Linear Display of active time per day
+Activity_Display(df)
